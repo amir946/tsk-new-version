395e4c049dff85edfff0a137018d59df03507f43
amirWebDev <amirgithub71@gmail.com>
2023-05-18 12:46:08 +0430

    fixx

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
-// import "./index.css";
 import { store } from "./store";
 import { Provider } from "react-redux";
 import { getAllcourses } from "./actions/courses";
